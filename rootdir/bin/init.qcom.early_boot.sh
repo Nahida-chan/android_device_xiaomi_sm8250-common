2279b82e2d7b465844f6bb09dd2097ec81bb2267
NextWork123 <nextdesigns@libero.it>
2022-09-14 10:07:03 +0545

    sm8250-common: rootdir: Disable Chimera.GmsIntentOperationService
    Signed-off-by: Sanjivns <113604406+Sanjivns@users.noreply.github.com>

--- rootdir/bin/init.qcom.early_boot.sh
+++ rootdir/bin/init.qcom.early_boot.sh
@@ -554,3 +554,4 @@ fi
 pm disable com.google.android.gms/com.google.android.gms.auth.managed.admin.DeviceAdminReceiver
 pm disable com.google.android.gms/com.google.android.gms.chimera.GmsIntentOperationService
 pm disable com.google.android.gms/com.google.android.gms.mdm.receivers.MdmDeviceAdminReceiver
+pm disable com.google.android.gms/.chimera.GmsIntentOperationService
