27e7d31e27b7db04bc5174927e4139036768e590
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-01-28 21:14:48 +0530

    sm8250-common: Silence some spammy logging
    HWC and cit sensor logspam is so intense it bumps up logd's idle CPU
    usage up to over 3%. With this change, logs are much cleaner and logd
    idle CPU usage is reduced to under 1%, while still allowing error logs
    to pass through.
    - Do this only in user/debug builds (except eng).
    - Add wifi and cnss too while we're at it, they can get noisy at times.

--- kona.mk
+++ kona.mk
@@ -266,6 +266,28 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+# Logging
+SPAMMY_LOG_TAGS := \
+    MiStcImpl \
+    SDM \
+    SDM-histogram \
+    SRE \
+    WifiHAL \
+    SensorService \
+    cnss-daemon \
+    libcitsensorservice@2.0-impl \
+    libsensor-displayalgo \
+    libsensor-parseRGB \
+    libsensor-ssccalapi \
+    sensors \
+    vendor.qti.hardware.display.composer-service \
+    vendor.xiaomi.sensor.citsensorservice@2.0-service
+
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+PRODUCT_VENDOR_PROPERTIES += \
+    $(foreach tag,$(SPAMMY_LOG_TAGS),log.tag.$(tag)=E)
+endif
+
 # Media configs
 PRODUCT_PACKAGES += \
     media_codecs_c2.xml \
@@ -363,8 +385,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl-xiaomi \
-    android.hardware.sensors@1.0-service \
-    SensorService
+    android.hardware.sensors@1.0-service
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
