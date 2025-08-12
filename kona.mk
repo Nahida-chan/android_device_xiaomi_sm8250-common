81312c5ff93d77d9d635f24cdbfa8089560c5b10
sashascurtu <sasha.scurtu@gmail.com>
2025-08-12 19:48:52 +0530

    sm8250-common: Build sensorservice 1.0 HAL
    Fixes:
    W HidlServiceManagement: Waited one second for android.frameworks.sensorservice@1.0::ISensorManager/default
    I hwservicemanager: Since android.frameworks.sensorservice@1.0::ISensorManager/default is not registered, trying to start it as a lazy HAL.
    I HidlServiceManagement: getService: Trying again for android.frameworks.sensorservice@1.0::ISensorManager/default...
    W libc    : Unable to set property ctl.interface_start to android.frameworks.sensorservice@1.0::ISensorManager/default: error code: 0x20
    
    Change-Id: If6fdef9dc39daaba9138fe3f8acf06cb6a0e78f8

--- kona.mk
+++ kona.mk
@@ -385,7 +385,8 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl-xiaomi \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    android.frameworks.sensorservice@1.0
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
