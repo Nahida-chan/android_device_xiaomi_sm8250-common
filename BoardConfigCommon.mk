5a718c0e06ff05c829147416c382650d33654327
yaseakun <agathagemink2014@gmail.com>
2025-10-28 12:33:13 +0000

    sm8250-common: Switch to use device frameworks matrix in tree

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -253,6 +253,7 @@ endif
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/configs/vintf/framework_compatibility_dolby.xml \
     $(COMMON_PATH)/configs/vintf/framework_compatibility_matrix.xml \
+    $(COMMON_PATH)/configs/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/vintf/manifest.xml
